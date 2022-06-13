5347bfec26be683639343228ed4cc6229c4dbf3f
Guillermo Leyendeker <gleyendeker@gmail.com>
2022-06-13 15:29:22 -0300

    set eth balance for the contract. Add a cooldown time to avoid spam

--- scripts/run.js
+++ scripts/run.js
@@ -3,18 +3,42 @@ const main = async () => {
     const [owner, randomPerson] = await hre.ethers.getSigners();
 
     const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
+    const waveContract = await waveContractFactory.deploy({
+        value: hre.ethers.utils.parseEther("0.01"),
+    });
     await waveContract.deployed();
     console.log("Contract deployed to:", waveContract.address);
     console.log("Contract deployed by:", owner.address);
+
+    /*
+   * Get Contract balance
+   */
+    let contractBalance = await hre.ethers.provider.getBalance(
+        waveContract.address
+    );
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
     console.log(waveCount.toNumber());
 
-
     let waveTxn = await waveContract.wave("owner message");
     await waveTxn.wait();
+
+    let waveTxn2 = await waveContract.wave("owner second message");
+    await waveTxn2.wait();
+
+    /*
+     * Get Contract balance to see what happened!
+     */
+    contractBalance = await hre.ethers.provider.getBalance(waveContract.address);
+    console.log(
+        "Contract balance:",
+        hre.ethers.utils.formatEther(contractBalance)
+    );
 
     waveTxn = await waveContract.connect(randomPerson).wave("random person message!");
     await waveTxn.wait();
